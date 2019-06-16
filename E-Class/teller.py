188b42da7f90c2a0cf9260daef3b73c477007ce5
HaneulYun <yhnil.kr@gmail.com>
2019-06-16 22:07:12 +0900

    텔레그램 : 기본 기능 구현 완료

--- E-Class/teller.py
+++ E-Class/teller.py
@@ -22,12 +22,13 @@ import noti
 
 
 items = []
+category = dict()
 
 def initData():
     conn = http.client.HTTPConnection("kocw.net")
     conn.request("GET",
          #"/home/api/handler.do?key=537adad829de4e65782196737ced103f35930363b8e30956&category_type=t&category_id=" + str(id+1) + "&from=" + str(self.dateStart) + "&to=" + str(self.dateEnd) + "&end_num=30000"
-         "/home/api/handler.do?key=537adad829de4e65782196737ced103f35930363b8e30956&category_type=t&category_id=1&from=20170101&to=20170201&end_num=10000"
+         "/home/api/handler.do?key=537adad829de4e65782196737ced103f35930363b8e30956&category_type=t&from=20170101&to=20180201&end_num=10000"
          #"/home/api/handler.do?key=537adad829de4e65782196737ced103f35930363b8e30956&from=20100101&to=20200201&end_num=30000"
          )
     req = conn.getresponse()
@@ -43,11 +44,14 @@ def initData():
         if 'course_title' in data.keys():
             items.append(data)
 
-    category = dict()
-
     for d in items:
+        global category
         if 'taxon' in d.keys():
             taxon = d['taxon'].split('>')
+            if taxon.__len__() < 2:
+                taxon.append(taxon[0])
+            if taxon.__len__() < 3:
+                taxon.append(taxon[1])
 
             if not taxon[0] in category:
                 category[taxon[0]] = dict()
@@ -96,6 +100,45 @@ def check( user ):
         row = 'id:' + str(data[0]) + ', location:' + data[1]
         noti.sendMessage( user, row )
 
+def printList0(user):
+    msg = ''
+    for d in category.keys():
+        msg += d+'\n'
+    if msg:
+        noti.sendMessage( user, msg )
+    else:
+        noti.sendMessage( user, '해당하는 데이터가 없습니다.')
+
+def printList1(user, key0):
+    msg = ''
+    for d in category[key0].keys():
+        msg += d+'\n'
+    if msg:
+        noti.sendMessage( user, msg )
+    else:
+        noti.sendMessage( user, '해당하는 데이터가 없습니다.')
+
+def printList2(user, key0, key1):
+    msg = ''
+    for d in category[key0][key1].keys():
+        msg += d+'\n'
+    if msg:
+        noti.sendMessage( user, msg )
+    else:
+        noti.sendMessage( user, '해당하는 데이터가 없습니다.')
+
+def printList3(user, key0, key1, key2):
+    msg = ''
+    for d in items:
+        if 'taxon' in d:
+            if d['taxon'] == key0+'>'+key1+'>'+key2:
+                msg += d['course_title']+'\n'
+                msg += d['provider']+'/'+d['lecturer']+'\n'
+                msg += d['course_url'] +'\n'
+    if msg:
+        noti.sendMessage( user, msg )
+    else:
+        noti.sendMessage( user, '해당하는 데이터가 없습니다.')
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
@@ -113,6 +156,17 @@ def handle(msg):
     elif text.startswith('확인'):
         print('try to 확인')
         check( chat_id )
+    #elif text.startswith('대분류'):
+    #    if len(args) > 1:
+    elif text.startswith('?'):
+        if len(args) == 1:
+            printList0(chat_id)
+        if len(args) == 2:
+            printList1(chat_id, args[1])
+        if len(args) == 3:
+            printList2(chat_id, args[1], args[2])
+        if len(args) == 4:
+            printList3(chat_id, args[1], args[2], args[3])
     else:
         noti.sendMessage(chat_id,
 """
