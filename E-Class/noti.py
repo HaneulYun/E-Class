51c74072a28e5d93445aea6b64cea9bc34b5869a
BaeHyoJeong <hammer456@naver.com>
2019-06-16 20:29:19 +0900

    텔레그램 임시
    텔레그램 임시

---
+++ E-Class/noti.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys
+import time
+import sqlite3
+import telepot
+from pprint import pprint
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+from datetime import date, datetime, timedelta
+import traceback
+
+
+import urllib
+import urllib.request
+import http.client
+import xml.etree.ElementTree as ET
+
+key = '803213420'
+TOKEN = '842313985:AAHbNWUKSPpAbLjeSmYhB2M23y0pjBIqEKo'
+MAX_MSG_LENGTH = 300
+baseurl = 'http://www.kocw.net/home/api/handler.do?key=537adad829de4e65782196737ced103f35930363b8e30956&from=20170101&to=20170201'
+
+bot = telepot.Bot(TOKEN)
+
+
+
+def getData(taxon, course_title):
+    res_list = []
+    url = baseurl+"&taxon="+taxon+"&course_title="+course_title
+    # print(url)
+    res_body = urlopen(url).read()
+    # print(res_body)
+    soup = BeautifulSoup(res_body, 'html.parser')
+    items = soup.findAll('item')
+    for item in items:
+        item = re.sub('<.*?>', '|', item.text)
+        parsed = item.split('|')
+        try:
+            row = parsed[8]
+        except IndexError:
+            row = item.replace('|', ',')
+        if row:
+            res_list.append(row.strip())
+    return res_list
+
+def sendMessage(user, msg):
+    try:
+        bot.sendMessage(user, msg)
+    except:
+        traceback.print_exc(file=sys.stdout)
+
+def run(date_param, param='11710'):
+    conn = sqlite3.connect('logs.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS logs( user TEXT, log TEXT, PRIMARY KEY(user, log) )')
+    conn.commit()
+
+    user_cursor = sqlite3.connect('users.db').cursor()
+    user_cursor.execute('CREATE TABLE IF NOT EXISTS users( user TEXT, location TEXT, PRIMARY KEY(user, location) )')
+    user_cursor.execute('SELECT * from users')
+
+    for data in user_cursor.fetchall():
+        user, param = data[0], data[1]
+        print(user, date_param, param)
+        res_list = getData( param, date_param )
+        msg = ''
+        for r in res_list:
+            try:
+                cursor.execute('INSERT INTO logs (user,log) VALUES ("%s", "%s")'%(user,r))
+            except sqlite3.IntegrityError:
+                # 이미 해당 데이터가 있다는 것을 의미합니다.
+                pass
+            else:
+                print( str(datetime.now()).split('.')[0], r )
+                if len(r+msg)+1>MAX_MSG_LENGTH:
+                    sendMessage( user, msg )
+                    msg = r+'\n'
+                else:
+                    msg += r+'\n'
+        if msg:
+            sendMessage( user, msg )
+    conn.commit()
+
+if __name__=='__main__':
+    today = date.today()
+    current_month = today.strftime('%Y%m')
+
+    print( '[',today,']received token :', TOKEN )
+
+    pprint( bot.getMe() )
+
+    run(current_month)
---
+++ E-Class/teller.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys
+import time
+import sqlite3
+import telepot
+from pprint import pprint
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+from datetime import date, datetime, timedelta
+import traceback
+
+import noti
+
+
+def replyAptData(taxon, user, course_title):
+    print(user, taxon, course_title)
+    res_list = noti.getData(taxon,course_title)
+    msg = ''
+    for r in res_list:
+        print( str(datetime.now()).split('.')[0], r )
+        if len(r+msg)+1>noti.MAX_MSG_LENGTH:
+            noti.sendMessage( user, msg )
+            msg = r+'\n'
+        else:
+            msg += r+'\n'
+    if msg:
+        noti.sendMessage( user, msg )
+    else:
+        noti.sendMessage( user, '해당하는 데이터가 없습니다.')
+
+def save( user, loc_param ):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS users( user TEXT, location TEXT, PRIMARY KEY(user, location) )')
+    try:
+        cursor.execute('INSERT INTO users(user, location) VALUES ("%s", "%s")' % (user, loc_param))
+    except sqlite3.IntegrityError:
+        noti.sendMessage( user, '이미 해당 정보가 저장되어 있습니다.' )
+        return
+    else:
+        noti.sendMessage( user, '저장되었습니다.' )
+        conn.commit()
+
+def check( user ):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS users( user TEXT, location TEXT, PRIMARY KEY(user, location) )')
+    cursor.execute('SELECT * from users WHERE user="%s"' % user)
+    for data in cursor.fetchall():
+        row = 'id:' + str(data[0]) + ', location:' + data[1]
+        noti.sendMessage( user, row )
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if content_type != 'text':
+        noti.sendMessage(chat_id, '난 텍스트 이외의 메시지는 처리하지 못해요.')
+        return
+
+    text = msg['text']
+    args = text.split(' ')
+
+    #검색 자연과학
+    if text.startswith('검색') and len(args)>1:
+        print('try to 검색', args[1])
+        replyAptData(chat_id, args[1],args[2])
+    elif text.startswith('확인'):
+        print('try to 확인')
+        check( chat_id )
+    else:
+        noti.sendMessage(chat_id, """모르는 명령어입니다.
+                                  n 사용가능 명령어
+                                  """)
+
+
+today = date.today()
+current_month = today.strftime('%Y%m')
+
+print( '[',today,']received token :', noti.TOKEN )
+
+bot = telepot.Bot(noti.TOKEN)
+pprint( bot.getMe() )
+
+bot.message_loop(handle)
+
+print('Listening...')
+
+while 1:
+  time.sleep(10)
