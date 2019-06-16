29163ecab2c92747b0aac9ba00a68ba222c04abc
BaeHyoJeong <hammer456@naver.com>
2019-06-17 05:06:43 +0900

    Setup.py
    e-class 바깥에 넣으세요 하늘님

---
+++ Setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+setup(
+    name="E-Class",
+    version="1.0",
+    classifiers=["E-Class"],
+    data_files=[("E-Class")],
+    packages=["E-Class"]
+)
