41b6465295bdce4f187b78e597057685a453e0c2
HaneulYun <yhnil.kr@gmail.com>
2019-06-16 16:11:43 +0900

    검색 영역 UI 조정

--- E-Class/E_Class.py
+++ E-Class/E_Class.py
@@ -32,16 +32,16 @@ class App:
         self.initBookmarkListArea()
         self.initBody()
 
-        ####내가 한 곳###
-        self.v=IntVar()
-        radio1=Radiobutton(self.tk,text="리스트",variable=self.v,value=1, command=self.initClassListArea)
-        radio1.pack()
-        radio1.place(x=10,y=170)
-
-        radio2 = Radiobutton(self.tk, text="썸네일", variable=self.v, value=2) #command 썸네일일 때 되게 해야함
-        radio2.pack()
-        radio2.place(x=80, y=170)
-        ###내가 한 곳###
+        # ####내가 한 곳###
+        # self.v=IntVar()
+        # radio1=Radiobutton(self.tk,text="리스트",variable=self.v,value=1, command=self.initClassListArea)
+        # radio1.pack()
+        # radio1.place(x=10,y=170)
+        # 
+        # radio2 = Radiobutton(self.tk, text="썸네일", variable=self.v, value=2) #command 썸네일일 때 되게 해야함
+        # radio2.pack()
+        # radio2.place(x=80, y=170)
+        # ###내가 한 곳###
 
 
     def searchClass(self):
@@ -136,23 +136,29 @@ class App:
 
         values=['인문과학', '사회과학', '공학', '자연과학',
                 '교육학', '의약학', '예술체육']
+
         self.searchComboBox1 = ttk.Combobox(self.searchingArea, width=8, values=values)
-        self.searchComboBox1.place(x=10,y=15)
+        self.searchComboBox1.place(x=5,y=5)
         self.searchComboBox2 = ttk.Combobox(self.searchingArea, width=8)
-        self.searchComboBox2.place(x=90, y=15)
+        self.searchComboBox2.place(x=85, y=5)
         self.searchComboBox3 = ttk.Combobox(self.searchingArea, width=8)
-        self.searchComboBox3.place(x=170, y=15)
+        self.searchComboBox3.place(x=165, y=5)
+
+        self.searchDateGuide = Label(self.searchingArea, text='기간', bg='green')
+        self.searchDateGuide.place(x=265, y=5)
+        self.searchDateStart = Entry(self.searchingArea, width=8)
+        self.searchDateStart.place(x=305, y=6)
+        self.searchDateEnd = Entry(self.searchingArea, width=8)
+        self.searchDateEnd.place(x=385, y=6)
 
         #combobox 3개
 
-        InputLabel = Entry(self.searchingArea, width=20, borderwidth=2, relief='ridge')
-        InputLabel.pack()
-        InputLabel.place(x=250, y=15)
-        #검색하는 박스
+        # InputLabel = Entry(self.searchingArea, width=20, borderwidth=2, relief='ridge')
+        # InputLabel.place(x=250, y=15)
+        # #검색하는 박스
 
-        SearchButton = Button(self.searchingArea, text="검색", command=self.searchClass)
-        SearchButton.pack()
-        SearchButton.place(x=410, y=13)
+        self.searchButton = Button(self.searchingArea, text="검색", width=6, command=self.searchClass)
+        self.searchButton.place(x=395, y=30)
         #검색 버튼
 
         self.searchComboBox1.set('주제 분류')
@@ -166,78 +172,66 @@ class App:
 
         self.classListBox=Listbox(self.classListArea, width=60, height=20, borderwidth=2,relief='ridge',
                            yscrollcommand=ClassListBoxScrollbar.set, selectmode=SINGLE)
+        
         #리스트박스일 경우
         self.classListBox.pack()
         self.classListBox.bind('<<ListboxSelect>>', self.selectClass)
 
     def initBookmarkListArea(self):
         self.bookmarkListArea = Frame(self.tk, bg='yellow')
-        self.bookmarkListArea.place(x=0, y=500, width=450, height=220)
+        self.bookmarkListArea.place(x=0, y=550, width=450, height=170)
 
     def initBody(self):
+        self.body = Frame(self.tk, bg='red')
+        self.body.place(x=450, y=100, width=630, height=620)
 
-        self.body = Frame(self.tk)
-        self.body.place(x=460, y=100, width=290, height=300)
-
-        self.body_image = None#=PhotoImage(file="e-class_logo.png")
-        self.body_label=Label(self.tk, image=self.body_image)
-        self.body_label.place(x=460, y=100)
-
-
+        self.body_image = None
+        self.body_label=Label(self.body, image=self.body_image)
+        self.body_label.place(x=0, y=0)
+        
         #이미지
-
         self.body2=Frame(self.tk,bg="green")
         self.body2.place(x=760, y=100, width=290,height=300)
-
+        
         self.body2box1=Label(self.tk,  width=15, height=4, text="주제분류")
-        self.body2box1.pack()
         self.body2box1.place(x=750,y=100)
         self.body2box2 = Label(self.tk, width=15, height=4, text="강의이름")
-        self.body2box2.pack()
         self.body2box2.place(x=750, y=160)
         self.body2box3 = Label(self.tk, width=15, height=4, text="교수자명")
-        self.body2box3.pack()
         self.body2box3.place(x=750, y=220)
         self.body2box4 = Label(self.tk, width=15, height=4, text="제공기관")
-        self.body2box4.pack()
         self.body2box4.place(x=750, y=280)
         self.body2box5 = Label(self.tk, width=15, height=4, text="강의학기")
-        self.body2box5.pack()
         self.body2box5.place(x=750, y=340)
         #주제분류, 강의이름, 강의자, 제공기관, 강의기간
-
+        
         self.body3=Frame(self.tk)
         self.body3.place(x=860, y=100, width=190, height=300)
         #그에 따른 실제 내용
-
+        
         self.body3box1 = Text(self.tk, width=27, height=4,borderwidth=2)
-        self.body3box1.pack()
         self.body3box1.place(x=860, y=100)
         self.body3box2 = Text(self.tk, width=27, height=4, borderwidth=2)
-        self.body3box2.pack()
         self.body3box2.place(x=860, y=160)
         self.body3box3 = Text(self.tk, width=27, height=4, borderwidth=2)
-        self.body3box3.pack()
         self.body3box3.place(x=860, y=220)
         self.body3box4 = Text(self.tk, width=27, height=4, borderwidth=2)
-        self.body3box4.pack()
         self.body3box4.place(x=860, y=280)
         self.body3box5 = Text(self.tk, width=27, height=4, borderwidth=2)
-        self.body3box5.pack()
         self.body3box5.place(x=860, y=340)
-
+        
         self.body4=Frame(self.tk, bg='pink')
         self.body4.place(x=460, y=410, width=600, height=300)
         #강의소개, 홈페이지 링크
-
+        
         BodyBoxScrollbar = Scrollbar(self.body4)
         BodyBoxScrollbar.pack(side=RIGHT, fill=Y)
         self.body4box = Text(self.body4, width=80, height=50, borderwidth=2, relief='ridge',
                            yscrollcommand=BodyBoxScrollbar.set)
         self.body4box.pack()
         self.body4box.place(x=20,y=0)
-
-
+        
+        
         button=Button(self.tk, width=15, text="홈페이지 링크 버튼",command=self.click_homepage)
         button.place(x=940,y=70)
         #홈페이지 링크 버튼
